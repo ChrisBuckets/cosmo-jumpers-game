c86a53752847f9d22dc040119570b3e019c29362
FishFr0g <chriswin12@gmail.com>
2022-01-14 02:10:45 -0800

    yo

--- sketch.js
+++ sketch.js
@@ -63,8 +63,8 @@ function preload() {
 
   buildAstronaut = {
     template: loadImage(`./astronauts/template/${astronaut["template"][0]}.png`),
-    helmet: loadImage(`./astronauts/helmet/${astronaut["helmet"][Math.floor(Math.random() * astronaut["helmet"].length)]}.png`),
     suit: loadImage(`./astronauts/suit/${astronaut["suit"][Math.floor(Math.random() * astronaut["suit"].length)]}.png`),
+    helmet: loadImage(`./astronauts/helmet/${astronaut["helmet"][Math.floor(Math.random() * astronaut["helmet"].length)]}.png`),
     shoes: loadImage(`./astronauts/shoes/${astronaut["shoes"][Math.floor(Math.random() * astronaut["shoes"].length)]}.png`),
     jetpack: loadImage(`./astronauts/jetpack/${astronaut["jetpack"][Math.floor(Math.random() * astronaut["jetpack"].length)]}.png`),
 
