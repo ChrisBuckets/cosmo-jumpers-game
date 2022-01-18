218971ba921892ae4f394f9a6a68d56497376915
FishFr0g <chriswin12@gmail.com>
2022-01-17 19:48:01 -0800

    yo

--- sketch.js
+++ sketch.js
@@ -100,7 +100,7 @@ function draw() {
     background(menu);
     //menu.show();
     //menu.position(0, 0);
-    rect(width * 0.17, height * 0.427, 400, 90);
+    //rect(width * 0.17, height * 0.427, 400, 90);
     //rect(width * 0.17, height * 0.6, 400, 90);
     //rect(width * 0.17, height * 0.77, 400, 90);
   }
